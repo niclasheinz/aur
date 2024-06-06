aed7b669acb41697149cc343e59307520b0196ee
Niclas Heinz <niclas.heinz@hpost.net>
2024-06-06 07:49:30 +0200

    Fixed some issues in Arduino-mit-Fernbedienung-steuern_Hinterniserkennung.ino

--- Projekte/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung.ino
+++ Projekte/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung.ino
@@ -172,6 +172,19 @@ digitalWrite(SENDEN, LOW);
         delay(500);
         digitalWrite(9, HIGH);
         digitalWrite(5, HIGH);
+        delay(2000);
+        Serial.println("stop all (Ein/Aus)");
+        Serial.println("stop Motor (3)");
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(10, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(4, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(2, LOW);
+        delay(1000);
 
         
 }
