9effb0a2ac223305045eca2165f08beb908681ea
Niclas Heinz <niclas.heinz@hpost.net>
2024-06-12 17:48:44 +0000

    #8 Added force stop functionality

--- Projekte/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung/Arduino-with-remote-control_obstacle-detection.ino
+++ Projekte/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung/Arduino-with-remote-control_obstacle-detection.ino
@@ -135,7 +135,7 @@ void bypass_right() { // function for bypass objects from right side
     Serial.println("Bypass right")
 }
 
-void force_stop(){
+void force_stop(){ // if distance to one of the sensors is lower than 40 -> force stop
   if (Distance_front < 40 || Distance_back < 40 || Distance_right < 40 || Distance_left < 40) {
     Serial.println("Executing force-stop in 1s");
   }
