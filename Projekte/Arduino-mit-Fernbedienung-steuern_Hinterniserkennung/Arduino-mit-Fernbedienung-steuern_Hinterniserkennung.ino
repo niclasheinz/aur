786ef3759f4a1325cbb7da4076dfe23b8d9eab90
Niclas Heinz <niclas.heinz@hpost.net>
2024-06-15 12:37:02 +0200

    #7 #4 Updated status message

--- Projekte/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung.ino
+++ Projekte/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung.ino
@@ -254,7 +254,7 @@ void detector_left() {
   Serial.println(Distance_left); // Print the sensor output
 
   if (Distance_left == 0) {
-    Serial.println("Hallo"); // Output "Hallo" if sensor value is 0
+    Serial.println("Distance to left side to near."); // Output "Hallo" if sensor value is 0
     bypass_left();
   }
 
@@ -270,7 +270,7 @@ void detector_rightt() {
   Serial.println(Distance_right); // Print the sensor output
 
   if (Distance_right == 0) {
-    Serial.println("Hallo"); // Output "Hallo" if sensor value is 0
+    Serial.println("Distance to right side to near."); // Output "Hallo" if sensor value is 0
     bypass_right();
   }
 
