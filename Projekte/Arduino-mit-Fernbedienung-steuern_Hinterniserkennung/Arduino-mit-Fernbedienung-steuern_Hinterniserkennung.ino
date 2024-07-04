be8edafb471252e84f5d0aa9b249b5a3e18afb79
Niclas Heinz <niclas.heinz@hpost.net>
2024-07-04 06:02:45 +0000

    #22 Updating ..

--- Projekte/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung.ino
+++ Projekte/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung/Arduino-mit-Fernbedienung-steuern_Hinterniserkennung.ino
@@ -70,9 +70,8 @@ void drive_forwards() { // driving forwards
        Serial.println("Drive forwards (2)");
         analogWrite(mo_li_1, 230);
         analogWrite(mo_re_2, 200);
-//      digitalWrite(mo_re_2, HIGH);
-        digitalWrite(mo_re_2, LOW);
-        digitalWrite(mo_re_1, LOW);
+        // digitalWrite(mo_re_2, LOW);
+        // digitalWrite(mo_re_1, LOW);
 }
 
 void drive_forwards_bypass() {
